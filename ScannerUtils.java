bb0c6d1ee50bc5d318981d54c2ec6c667b542e38
Katze789 <mss7@outlook.com>
2025-02-06 18:46:57 +0100

    Update ScannerUtils.java

--- ScannerUtils.java
+++ ScannerUtils.java
@@ -11,31 +11,64 @@ import java.util.Scanner;
  */
 public class ScannerUtils {
     private static final Scanner scanner = new Scanner(System.in); //Declaro el atributo scanner
+    
     //Declaro el método para leer un entero
-    public static  int leerEntero(String mensaje) { 
-        System.out.println(mensaje); //Va a imprimir el mensaje que escriba en el .java donde importe esta librería
-        int numero = scanner.nextInt();
-        scanner.nextLine(); //Esto sirve para consumir el salto de línea y que no lo lea el sacanner de texto e imprima un espacio en blanco
+    public static  int leerEntero(String mensaje) {
+        int numero = 0; //Declaro la variable, le doy un valor para iniciarla y que no me de error más tarde
+        boolean entradaCorrecta; //Declaro este booleano para el bucle, cuando se cumpla, saldrá del bucle
+        do { 
+            entradaCorrecta = true; //Le damos el valor verdadero de entrada
+            try {
+                System.out.println(mensaje); //Va a imprimir el mensaje que escriba en el .java donde importe esta librería
+                numero = scanner.nextInt();
+                scanner.nextLine(); //Limpia el buffer y hace que leerCadena no imprima un espacio en blanco
+            } catch (Exception e) {
+                System.out.println("No se aceptan letras u otros símbolos. Por favor, pruebe de nuevo.");
+                scanner.nextLine(); //Limpia el buffer y evita que entre en bucle infinito
+                entradaCorrecta = false; //Si captura la excepción, vuelve la variable falsa y entra en el bucle
+            }
+        } while (!entradaCorrecta); //Mientras la variable sea falsa, seguirá el bucle
         return numero;
     }
     //Declaro el método para leer un decimal
     public static  double leerDecimal(String mensaje) {
-        System.out.println(mensaje);
-        double decimal = scanner.nextDouble();
-        scanner.nextLine();       
+        double decimal = 0.0;
+        boolean entradaCorrecta;
+        do {
+            entradaCorrecta = true;
+            try { //Usamos try...catch para manejar las excepciones
+                System.out.println(mensaje);
+                decimal = scanner.nextDouble();
+                scanner.nextLine(); //Limpia el buffer   
+            } catch (Exception e) {
+                System.out.println("No se aceptan letras u otros símbolos.\nUse ',' como coma. Por favor, pruebe de nuevo.");
+                scanner.nextLine(); //Limpia el buffer
+                entradaCorrecta = false;
+            }
+        } while (!entradaCorrecta);
         return decimal;
     }
     //Declaro el método para leer una cadena de texto
-    public static  String leerCadena(String mensaje) {
+    public static  String leerCadena(String mensaje) { //Al ser cadena de texto, admite cualquier símbolo o número, no hay que manejar excepciones
         System.out.println(mensaje);
         String texto = scanner.nextLine();
         return texto;
     }
     //Declaro el método para leer un booleano
     public static  boolean leerBooleano(String mensaje) {
-        System.out.println(mensaje);
-        boolean respuesta = scanner.nextBoolean();
+        boolean respuesta = true;
+        boolean entradaCorrecta;
+        do {
+            entradaCorrecta = true;
+            try {
+                System.out.println(mensaje);
+                respuesta = scanner.nextBoolean();
+            } catch (Exception e) {
+                System.out.println("Se aceptan como entradas válidas: true o false. Por favor, pruebe de nuevo.");
+                scanner.nextLine(); //Limpia el buffer
+                entradaCorrecta = false;
+            }
+        } while (!entradaCorrecta);
         return respuesta;
     }
- 
 }
